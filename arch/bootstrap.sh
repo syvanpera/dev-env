c3b93df33c5fef681a0015cc524d419475c13efe
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2019-10-21 13:52:43 +0300

    Install AUR helper

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -5,3 +5,6 @@ sudo pacman -Syu --noconfirm
 
 # Install Ansible
 sudo pacman -S ansible --noconfirm --needed
+
+# Install AUR module for Ansible
+git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
